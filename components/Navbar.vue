5bde6a17f81818bf8489bea7fbe51cc8cdfd7cd8
DrigaNicolai <just.whyme7@gmail.com>
2022-07-18 13:13:05 +0300

    Home page

---
+++ components/Navbar.vue
@@ -0,0 +1,64 @@
+<script>
+export default {
+  name: "Navbar"
+}
+</script>
+
+<template>
+
+  <nav class="bg-gray-200 border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+    <div class="container flex flex-wrap justify-between items-center mx-auto">
+      <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-lg md:font-medium w-full items-center">
+        <li>
+          <nuxt-link
+            to="/"
+            class="
+              block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white
+            "
+            aria-current="page"
+          >Home</nuxt-link>
+        </li>
+        <li>
+          <a
+            href="#"
+            class="
+              block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent
+              md:border-0 md:hover:text-blue-700 md:p-0
+            "
+          >About</a>
+        </li>
+        <li>
+          <a
+            href="#"
+            class="
+              block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent
+              md:border-0 md:hover:text-blue-700 md:p-0
+            "
+          >Goods</a>
+        </li>
+        <li>
+          <a
+            href="#"
+            class="
+              block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent
+              md:border-0 md:hover:text-blue-700 md:p-0
+            "
+          >Login</a>
+        </li>
+        <li>
+          <a
+            href="#"
+            class="
+              block py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 md:hover:bg-transparent md:border-0
+              md:hover:text-blue-700 md:p-0
+            "
+          >Logout</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+</template>
+
+
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,19 @@
 <template>
-  <Tutorial />
+  <div class="mt-10 mx-10">
+    <h1 class="text-4xl font-bold mb-10">This is the home page of my little test project</h1>
+    <p class="mt-5 text-lg">
+      This is my try to implement my knowledge about <span class="text-green-300 font-bold">Nuxt.js</span>
+      and <span class="text-blue-300 font-bold">Tailwind</span> in one simple project.
+    </p>
+    <p class="mt-5 text-lg">
+      This project has not so many pages or functions, but it is like an example and practical task to cope with
+      new framework and CSS styles library.
+    </p>
+    <p class="mt-5 text-lg">
+      Please <span class="font-bold">Login</span> first to get access to <span class="text-blue-600">About</span> and
+      <span class="text-blue-600">Goods</span> pages.
+    </p>
+  </div>
 </template>
 
 <script>
