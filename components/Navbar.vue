adf70f7ff47887b9b6d3cc18c409b6a8f4f3dd4f
DrigaNicolai <just.whyme7@gmail.com>
2022-07-18 13:34:24 +0300

    About page

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -5,7 +5,6 @@ export default {
 </script>
 
 <template>
-
   <nav class="bg-gray-200 border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
     <div class="container flex flex-wrap justify-between items-center mx-auto">
       <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-lg md:font-medium w-full items-center">
@@ -13,19 +12,22 @@ export default {
           <nuxt-link
             to="/"
             class="
-              block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white
+              block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent
+              md:border-0 md:hover:text-blue-700 md:p-0
             "
-            aria-current="page"
+            exact
+            active-class="text-indigo-600"
           >Home</nuxt-link>
         </li>
         <li>
-          <a
-            href="#"
+          <nuxt-link
+            to="/about"
             class="
               block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent
               md:border-0 md:hover:text-blue-700 md:p-0
             "
-          >About</a>
+            active-class="text-indigo-600"
+          >About</nuxt-link>
         </li>
         <li>
           <a
@@ -57,8 +59,6 @@ export default {
       </ul>
     </div>
   </nav>
-
 </template>
-
 
 <style scoped></style>
---
+++ pages/about.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  name: "about"
+}
+</script>
+
+<template>
+  <div class="mt-10 mx-10">
+    <h1 class="text-4xl font-bold mb-10">About page</h1>
+    <p class="mt-5 text-lg">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad commodi consequatur culpa id
+      natus nulla perferendis praesentium quaerat veniam. Aliquam aut cumque dolore eum facilis, labore magni quasi
+      tempore. Repudiandae.
+    </p>
+    <p class="mt-5 text-lg">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad commodi consequatur culpa id
+      natus nulla perferendis praesentium quaerat veniam. Aliquam aut cumque dolore eum facilis, labore magni quasi
+      tempore. Repudiandae.
+    </p>
+    <p class="mt-5 text-lg">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda deserunt minus nesciunt officia officiis quas
+      quos rem suscipit velit voluptates?
+    </p>
+  </div>
+</template>
+
+
