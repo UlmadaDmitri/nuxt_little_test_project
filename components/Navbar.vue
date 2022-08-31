230ef8a6d36e2b2adcbbf21f2d149030aa615cec
DrigaNicolai <just.whyme7@gmail.com>
2022-08-31 12:05:33 +0300

    Navbar logo changes

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -9,7 +9,7 @@ export default {
     <nav class="bg-white border-gray-200 dark:bg-gray-900">
       <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-2.5">
         <div class="flex items-center">
-          <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
+          <img src="@/assets/nuxt-logo.svg" class="mr-3 h-6 sm:h-9" alt="Nuxt Logo" />
           <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">My Project</span>
         </div>
         <div class="flex justify-between items-center">
