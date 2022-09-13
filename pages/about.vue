cc7763443cbb112d77f67cedded9c4d847487f21
DrigaNicolai <just.whyme7@gmail.com>
2022-09-13 13:14:27 +0300

    About page

--- pages/about.vue
+++ pages/about.vue
@@ -37,6 +37,7 @@ export default {
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">NuxtJS</h5>
             <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
               It is the intuitive Vue Framework. It offers file-system routing, data fetching and it is SEO friendly.
+              In some aspects it is much better than simple Vue.
             </p>
             <a
               href="https://nuxtjs.org/"
@@ -81,6 +82,46 @@ export default {
           </div>
         </div>
       </div>
+    </div>
+    <div class="max-w-screen-xl px-2 py-4 mx-auto lg:py-8">
+      <h2 class="text-4xl font-extrabold dark:text-white">History</h2>
+      <ol class="relative border-l border-gray-200 dark:border-gray-700 my-4 lg:my-8">
+        <li class="mb-10 ml-6">
+        <span class="flex absolute -left-3 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+            <svg aria-hidden="true" class="w-3 h-3 text-blue-600 dark:text-blue-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+        </span>
+          <h3 class="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+            First pages
+            <span class="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">Latest</span>
+          </h3>
+          <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">September 7th, 2022</time>
+          <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+            Were released first two pages. Home page with the general information and About page with the more detailed
+            information about this project.
+          </p>
+        </li>
+        <li class="mb-10 ml-6">
+        <span class="flex absolute -left-3 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+          <svg aria-hidden="true" class="w-3 h-3 text-blue-600 dark:text-blue-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+        </span>
+          <h3 class="mb-1 text-lg font-semibold text-gray-900 dark:text-white">NuxtJS and Tailwind learning</h3>
+          <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">July, 2022 - August, 2022</time>
+          <p class="text-base font-normal text-gray-500 dark:text-gray-400">
+            Getting acquainted with the new framework and style library. First steps to implement them both in the project.
+          </p>
+        </li>
+        <li class="ml-6">
+        <span class="flex absolute -left-3 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+            <svg aria-hidden="true" class="w-3 h-3 text-blue-600 dark:text-blue-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+        </span>
+          <h3 class="mb-1 text-lg font-semibold text-gray-900 dark:text-white">Project Initialization</h3>
+          <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">July 18th, 2022</time>
+          <p class="text-base font-normal text-gray-500 dark:text-gray-400">
+            The blank  project was created.
+          </p>
+        </li>
+      </ol>
+
     </div>
     <!-- https://flowbite.com/docs/components/timeline/ -->
   </div>
