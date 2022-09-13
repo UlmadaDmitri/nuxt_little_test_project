9154c54083b948b3170a4e4676fd3c1d6977f0e9
DrigaNicolai <just.whyme7@gmail.com>
2022-09-13 13:57:34 +0300

    Error (WIP)

---
+++ layouts/error.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="py-4 px-2 mx-auto max-w-screen-xl text-center lg:py-8 lg:px-6">
+    <h1 class="mb-4 text-4xl font-extrabold text-red-500 md:text-5xl lg:text-6xl dark:text-white">
+      Error
+    </h1>
+    <p class="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
+      This page doesn't exist. Please check the address you try to connect.
+    </p>
+  </div>
+</template>
