df6d3427ebf226ccbb57af04d90d00b7c9440fa0
DrigaNicolai <just.whyme7@gmail.com>
2023-01-05 17:54:16 +0200

    Change title

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'little_test_project',
+    title: 'TestProject',
     htmlAttrs: {
       lang: 'en'
     },
