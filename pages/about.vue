172c3f801b001308424b95a14321c48e66f26cca
DrigaNicolai <just.whyme7@gmail.com>
2023-01-05 18:00:39 +0200

    Update about page(history)

--- pages/about.vue
+++ pages/about.vue
@@ -94,6 +94,19 @@ export default {
             First pages
             <span class="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">Latest</span>
           </h3>
+          <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">January 5th, 2023</time>
+          <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+            After a long break and several video tutorias(to refresh Nuxtjs knowledge I have) login page with
+            the $store features has been released
+          </p>
+        </li>
+        <li class="mb-10 ml-6">
+        <span class="flex absolute -left-3 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+            <svg aria-hidden="true" class="w-3 h-3 text-blue-600 dark:text-blue-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+        </span>
+          <h3 class="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+            First pages
+          </h3>
           <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">September 7th, 2022</time>
           <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
             Were released first two pages. Home page with the general information and About page with the more detailed
