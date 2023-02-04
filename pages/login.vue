9dc31b81e22acc854aedac53d9aebca608212015
DrigaNicolai <just.whyme7@gmail.com>
2023-02-04 18:54:51 +0200

    Add warning alert

--- pages/login.vue
+++ pages/login.vue
@@ -1,6 +1,29 @@
 <template>
   <section class="bg-gray-50 dark:bg-gray-900">
     <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+      <div
+        v-if="$route.query.message"
+        class="
+          flex p-4 mb-4 text-sm text-yellow-800 rounded-lg
+          bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300 dark:border-yellow-800
+        "
+        role="alert"
+      >
+        <svg
+          aria-hidden="true"
+          class="flex-shrink-0 inline w-5 h-5 mr-3"
+          fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+        <div>
+          <span class="font-medium">Warning alert!</span> Need login first
+        </div>
+      </div>
       <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
         <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
           <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
@@ -81,6 +104,7 @@ export default {
         this.$store.dispatch("auth/login");
         this.$router.push("/");
       } else {
+        this.$router.push("/login");
         this.errorMessage = "Invalid credentials. Please, enter the correct email and password";
       }
     }
