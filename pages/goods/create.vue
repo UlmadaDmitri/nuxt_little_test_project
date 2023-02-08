4a0a8f9e4d67be5a1692ffaa65f3fe6800d68ec0
DrigaNicolai <just.whyme7@gmail.com>
2023-02-08 11:33:01 +0200

    Create page and edit page layout

--- pages/goods/create.vue
+++ pages/goods/create.vue
@@ -4,22 +4,6 @@ export default {
 
   // middleware: ["auth"],
 
-  validate({params}) {
-    /*if (!(/^\d+$/.test(params.id))) {
-      throw new Error("Not valid params");
-    }
-
-    return true;*/
-    return /^\d+$/.test(params.id);
-  },
-
-  async asyncData({$axios, params}) {
-    const response = await $axios.get(`https://61ea7b5d7bc0550017bc677c.mockapi.io/api/v1/goods/${params.id}`);
-    const good = response.data;
-
-    return {good};
-  },
-
   methods: {
     goBack() {
       this.$router.push("/goods");
