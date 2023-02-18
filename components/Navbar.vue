7c60a6e0c4abe4c66bd3d298b48f6c5241208b34
DrigaNicolai <just.whyme7@gmail.com>
2023-02-18 11:04:52 +0200

    Add active class for goods link

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -80,6 +80,7 @@ export default {
                 block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent
                 md:border-0 md:hover:text-blue-700 md:p-0
               "
+              active-class="text-indigo-600"
             >Goods</nuxt-link>
           </li>
         </ul>
